c67411c4cac97adc887aeac45f1187516b4482cf
Rafael Lerm <rafaellerm@gmail.com>
2015-04-13 23:59:12 -0300

    Adds hack to allow moving fullscreen windows.

--- src/mdptvp/gui/util.cpp
+++ src/mdptvp/gui/util.cpp
@@ -12,5 +12,14 @@ void gui::moveToScreen(QWidget* widget, int screen_number) {
     screen_number = -1;
   }
 
+  // Moving between desktops doesn't work for fullscreen windows, so we use this
+  // ugly hack. We un-fullscreen the widget, move it, and then restore the
+  // previous window state. For windows that are not fullscreen, this has no
+  // effect since the previous state is restored.
+  Qt::WindowStates old_state = widget->windowState();
+  widget->setWindowState(widget->windowState() & ~Qt::WindowFullScreen);
+
   widget->move(desktop->availableGeometry(screen_number).topLeft());
+
+  widget->setWindowState(old_state);
 }
