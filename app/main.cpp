e4d4c12eafb094095a9a69a88eb166a61c938d4a
Rafael Lerm <rafaellerm@gmail.com>
2015-04-17 00:04:57 -0300

    Adds initialization of qsettings module.

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,13 @@
 #include "mdptvp/mainwindow.h"
 
-#include <QApplication>
+#include <QtCore/QCoreApplication>
+#include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setOrganizationName("MDPT");
+    QCoreApplication::setApplicationName("MDPT Video Player");
+
     QApplication a(argc, argv);
     mdptvp::MainWindow w;
     w.show();
